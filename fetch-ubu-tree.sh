a612bb8229a4498b3822ce5c28691a316d3ac72b
untoreh <bassobassista@gmail.com>
2017-05-17 16:39:38 +0200

    .
    Former-commit-id: 39d4f5b06d23a2d3126cd1a80553acbab4d56b41

--- fetch-ubu-tree.sh
+++ fetch-ubu-tree.sh
@@ -6,9 +6,10 @@ remote_repo="untoreh/trub"
 artifact="trub.tar"
 m_path="target"
 dest_path="release"
+ref="trunk"
 
 ## sanity check
-if [ $(find target/* -type d -maxdepth 0 | wc -l) -eq 0 ]; then
+if [ $(find target/* -type d -maxdepth 0 2>/dev/null | wc -l) -eq 0 ]; then
     ## fetch img into release folder, beware the slash
     fetch_artifact $remote_repo /$artifact $dest_path
 fi
@@ -18,6 +19,9 @@ source repo.sh bare-user
 cmt=$(b64name $dest_path)
 ## apply the delta
 ostree --repo=${repo} static-delta apply-offline $dest_path/$cmt
+## tag the delta
+ostree --repo=${repo} refs --delete ${ref}
+ostree --repo=${repo} refs --create=${ref} $cmt
 
 ## we now have the base ubuntu repo
 echo $(realpath $repo)
