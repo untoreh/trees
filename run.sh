d5e25f8d1bfc811ad23a5b50c5082f62dbf5b86a
untoreh <bassobassista@gmail.com>
2017-06-07 20:57:59 +0200

    staged travis build

--- run.sh
+++ run.sh
@@ -8,6 +8,10 @@ if [ ! $PKG ]; then
     printc "no PKG specified for the build process, terminating."
     exit 1
 fi
+if [ $(find ${PKG}*.tar | wc -l) -gt 0 ]; then
+    printc "package built, continuing..."
+    exit
+fi
 
 ## build app
 ./apps/$PKG/$PKG
