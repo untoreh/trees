cf7a630afbd938fc777b78e33cc9113522bd72cc
untoreh <bassobassista@gmail.com>
2017-06-09 07:29:01 +0200

    .

--- functions.sh
+++ functions.sh
@@ -129,6 +129,16 @@ release_older_than() {
     fi
 }
 
+## get mostly local vars
+diff_env(){
+    bash -cl "set -o posix && set >/tmp/clean.env"
+    set -o posix && set >/tmp/local.env && set +o posix
+
+    diff /tmp/clean.env /tmp/local.env | grep -E "^>|^\+" | \
+        grep -Ev "^(>|\+) ?(BASH|COLUMNS|LINES|HIST|PPID|SHLVL|PS(1|2)|SHELL|FUNC)" | \
+        sed -r 's/^> ?|^\+ ?//'
+}
+
 ## $1 repo:tag
 ## $2 artifact name
 ## $3 dest dir
